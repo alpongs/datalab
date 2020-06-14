81bb5b54f2d4e657c69d7e8fad72208a917bb6e1
I. YOON <tingcobell@gmail.com>
2020-06-14 22:35:28 +0900

    Queue 예제 구성.

--- src/main/java/datalab/QueueExample.java
+++ src/main/java/datalab/QueueExample.java
@@ -1,5 +1,32 @@
 package datalab;
 
+class Queue<T> {
+    class Node<T> {
+        T item;
+        Node<T> next;
+
+        public Node(T item, Node<T> next) {
+            this.item = item;
+            this.next = next;
+        }
+    }
+
+    private Node<T> first;
+    private Node<T> last;
+
+    public void push(T item) {
+
+    }
+
+    public T pop() {
+        return null;
+    }
+
+    public T peek() {
+        return null;
+    }
+}
+
 public class QueueExample {
 
 }
