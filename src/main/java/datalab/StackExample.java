4650468f54ed4208234cb719a5b17d51198c6ca0
I. YOON <tingcobell@gmail.com>
2020-06-14 22:23:20 +0900

    Stack 학습을 위한 기본 클래스 생성.

--- src/main/java/datalab/StackExample.java
+++ src/main/java/datalab/StackExample.java
@@ -1,5 +1,54 @@
 package datalab;
 
+class Stack<T> {
+
+    class Node<T> {
+
+        private T data;
+        private Node<T> next;
+
+        public Node(T data) {
+            this.data = data;
+            this.next = null;
+        }
+
+        public T getData() {
+            return data;
+        }
+
+        public Node<T> getNext() {
+            return next;
+        }
+
+        public void setNext(Node<T> next) {
+            this.next = next;
+        }
+    }
+
+    private Node<T> top;
+
+    public T pop() {
+        return null;
+    }
+
+    public void push(T item) {
+
+    }
+
+    public T peek() {
+        return null;
+    }
+
+    public boolean isEmpty() {
+        return true;
+    }
+
+    public int size() {
+        return 0;
+    }
+
+}
+
 public class StackExample {
 
 }
