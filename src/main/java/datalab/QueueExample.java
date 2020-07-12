ffb4d94b2b920b400f26e2b79d6927096e5ea35c
jhan94 <56765156+jhan94@users.noreply.github.com>
2020-07-12 22:54:41 +0900

    q_ push/pop 구현 2

--- src/main/java/datalab/QueueExample.java
+++ src/main/java/datalab/QueueExample.java
@@ -1,23 +1,25 @@
 package datalab;
 
+
+
 import java.util.EmptyStackException;
 
-class Queue {
-    class Node {
-        int item;
-        Node next;
+class Queue<T> {
+    class Node<T> {
+        T item;
+        Node<T> next;
 
-        public Node(int item) {
+        public Node(T item) {
             this.item = item;
             this.next = null;
         }
     }
 
-    private Node first;
-    private Node last;
+    private Node<T> first;
+    private Node<T> last;
 
-    public void push(int item) {
-        Node newNode = new Node(item);
+    public void push(T item) {
+        Node newNode = new Node<>(item);
         if(last ==null){
             first = newNode;
         }
@@ -28,29 +30,42 @@ class Queue {
         last = newNode;
     }
 
-    public int pop() {
+    public T pop() {
 
         if( first == null){
             throw new EmptyStackException();
         }
-        Node poped = first;
+        Node<T> poped = first;
         first = poped.next;
 
         return poped.item;
     }
 
-    public int peek() {
-
-        return 0;
-    }
-    public int size(){
-        int size = 0;
-        while(first!=null){
-            size++;
-            first = first.next;
+    public T peek() {
+        if(first==null){
+            throw new EmptyStackException();
         }
-        return size;
+
+        return first.item;
     }
+    public boolean isEmpty(){
+        if(first == null){
+            return true;
+        }
+        return false;
+    }
+    public int size() {
+        int total = 0;
+
+        Node<T> count = first;
+        while (count != null) {
+            total++;
+            count = count.next;
+        }
+
+        return total;
+    }
+
 }
 
 public class QueueExample {
--- src/test/java/datalab/QueueTest.java
+++ src/test/java/datalab/QueueTest.java
@@ -8,7 +8,8 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class QueueTest {
-    Queue q = null;
+    Queue<Integer> q = null;
+
     @BeforeEach
     void setUp() {
         q = new Queue();
@@ -21,23 +22,43 @@ class QueueTest {
     void tearDown() {
         q = null;
     }
+
     @Test
     void push() {
         System.out.println("push");
-        Assertions.assertEquals(5,q.size());
+        Assertions.assertEquals(5, q.size());
 
     }
 
     @Test
     void pop() {
         Integer p1 = q.pop();
-        Assertions.assertEquals(0,p1);
-        Assertions.assertEquals(4,q.size());
+        Assertions.assertEquals(0, p1);
+        Assertions.assertEquals(4, q.size());
 
         Integer p2 = q.pop();
         Assertions.assertEquals(1, p2);
         Assertions.assertEquals(3, q.size());
 
 
+    }
+
+    @Test
+    void peek() {
+        Integer pk = q.peek();
+        Assertions.assertEquals(0, pk);
+        q.pop();
+        Integer pk2 = q.peek();
+        Assertions.assertEquals(1, pk2);
+    }
+
+    @Test
+    void isEmpty() {
+        q.pop();
+        q.pop();
+        q.pop();
+        q.pop();
+        q.pop();
+        Assertions.assertTrue(q.isEmpty());
     }
 }
